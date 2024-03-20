7c238b857dc88155f1af119377d70847c83af30f
briimiin <kipkorirbrimin005@gmail.com>
2024-03-20 23:18:41 +0300

    arrow functions lab

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = function() {
+    return 2000 / 100;
+}
+
+// Arrow function called square
+const square = (num) => {
+    return num * num;
+}
+
+// Arrow function called add
+const add = (a, b) => {
+    return a + b;
+}
+
+// Testing the functions
+console.log(divide()); // Output: 20
+console.log(square(5)); // Output: 25
+console.log(add(5, 10)); // Output: 15
